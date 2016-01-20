3e6ea8ed46f635a50fd6aa762001b3a849dd9a6e
Adam Ross Russell and matt knutson <mknut001@gmail.com>
2016-01-20 14:29:01 -0800

    add functionality for one word

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,54 @@
-var pigLatin = function(englishWord) {
-  if (englishWord[0] === 'a' || englishWord[0] === 'e' || englishWord[0] === 'i' || englishWord[0] === 'o' || englishWord[0] === 'u') {
-  var translatedWord = englishWord;
-} else {
-  var translatedWord = englishWord.slice(1) + englishWord[0];
+var indexOfFirstVowel = function(word) {
+
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  var letters = word.split("");
+
+  for (var i = 0; i < letters.length; i++) {
+    for (var j = 0; j < vowels.length; j++) {
+      if (letters[i] === vowels[j]) {
+        return i;
+      }
+    }
+  }
 }
-  return translatedWord + "ay";
-};
+
+  var pigLatin = function(englishWord) {
+  var splitAt = indexOfFirstVowel(englishWord);
+  return englishWord.slice(splitAt) + englishWord.slice(0, splitAt) + 'ay';
+}
+
+
+//   if (!hasConsonantAt(englishWord, 0)) {
+//   var translatedWord = englishWord;
+// }
+//   else if (hasConsonantAt(englishWord, 1)) {
+//   var translatedWord = englishWord.slice(2) + englishWord.slice(0, 2);
+// }
+//   else if (hasConsonantAt(englishWord, 0)) {
+//   var translatedWord = englishWord.slice(1) + englishWord.slice(0, 1);
+// }
+//   return translatedWord + "ay";
+// };
+
+
+// var hasConsonantAt = function(word, position) {
+//   var vowels = ['a', 'e', 'i', 'o', 'u'];
+//   for (var i = 0; i < vowels.length; i++) {
+//     if (word[position] === vowels[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+$(document).ready(function(event) {
+    $("form#pigLatin").submit(function(event){
+
+      var word = $("input#message").val();
+      var result = pigLatin(word);
+
+  $(".result").text(result);
+  $("#result").show();
+  event.preventDefault();
+
+});
+});
--- js/specs.js
+++ js/specs.js
@@ -1,10 +1,33 @@
 describe("pigLatin", function() {
-  it("adds 'ay' to the end of a word that starts with a vowel", function() {
+  it("adds 'ay' to the end of a word that starts with an a", function() {
     expect(pigLatin("apple")).to.equal("appleay");
   });
-
+  it("adds 'ay' to the end of a word that starts with a vowel", function() {
+    expect(pigLatin("english")).to.equal("englishay");
+  });
   it("takes a single consonant at the beginning of the word and moves it to the end", function () {
     expect(pigLatin("cat")).to.equal("atcay");
+  });
+  it("takes two consonants at the beginning of a word and moves them to the end", function () {
+      expect(pigLatin("cricket")).to.equal("icketcray");
+  });
+});
 
- });
+// describe("hasConsonantAt", function() {
+//   it("returns true if a word has a consonant in the specified spot", function() {
+//     expect(hasConsonantAt("peanut", 0)).to.equal(true);
+//   });
+//   it("returns false if a word does not have a consonsant in the specified spot", function() {
+//     expect(hasConsonantAt("peanut", 1)).to.equal(false);
+//   });
+// });
+
+describe("indexOfFirstVowel", function() {
+  it("returns 0 for a word that starts with a vowel", function() {
+    expect(indexOfFirstVowel("apple")).to.equal(0);
+  });
+
+  it("returns 1 for a word that starts with a single consonsant", function() {
+    expect(indexOfFirstVowel("cat")).to.equal(1);
+  });
 });
